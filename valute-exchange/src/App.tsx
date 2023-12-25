51b6690aeb35d03a9fbda5c100aa07cd7bc8a3a0
Vladislav Shalaev <shalaev.v@yandex.ru>
2023-12-25 17:17:06 +0300

    currency

--- valute-exchange/src/App.tsx
+++ valute-exchange/src/App.tsx
@@ -1,14 +1,17 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Currency from "./components/currency/Currency";
+import { Navbar } from "./components/navbar/NavBar"
+import { Route, Routes } from 'react-router-dom';
 function App() {
  
 
   return (
     <>
-     
+       <Navbar/>
+   <Routes>
+    <Route path='/' element={ <Currency/>} />
+    {/* <Route path='/charts' element={<ChartPages/>} />
+    <Route path='/exchange' element={<Exchange/>} /> */}
+   </Routes>
     </>
   )
 }
